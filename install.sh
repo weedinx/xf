eb615929ac5cbecef0b8b4ee54013dc346c165b0
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-18 15:26:26 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,14 +19,14 @@ sudo apt install -y network-manager-gnome
 
 sudo apt install -y linux-firmware
 sudo apt install -y xserver-xorg xserver-xorg-core xfonts-base xinit x11-xserver-utils
-sudo apt install -y xfwm4 xfce4-panel xfce4-settings xfce4-session xfce4-terminal xfdesktop4 xfce4-taskmanager tango-icon-theme xfce4-screenshooter xfce4-power-manager gtk2-engines gtk2-engines-pixbuf
+sudo apt install -y xfwm4 xfce4-panel xfce4-settings xfce4-session xfce4-terminal xfdesktop4 xfce4-taskmanager tango-icon-theme xfce4-screenshooter xfce4-power-manager gtk2-engines policykit-1 gtk2-engines-pixbuf thunar-data thunar-volman thunar xfce4-terminal thunar-media-tags-plugin thunar-archive-plugin xfce4-whiskermenu-plugin
 
-mv cp etc/apt/apt.conf etc/apt/apt.conf.a 
+mv /etc/apt/apt.conf /etc/apt/apt.conf.a 
 
-sudo apt install -y gvfs gvfs-fuse gvfs-backends policykit-1 udisks2
+sudo apt install -y gvfs gvfs-fuse gvfs-backends udisks2
 
 sudo apt install -y dkms curl git htop viewnior cpu-x gpick yaru-theme-gtk yaru-theme-icon pavucontrol pulseaudio-utils pulseaudio cups system-config-printer printer-driver-gutenprint printer-driver-escpr cups-client hplip -y
-sudo apt install -y thunar-data thunar-volman thunar xfce4-terminal thunar-media-tags-plugin thunar-archive-plugin xfce4-whiskermenu-plugin
+#sudo apt install -y thunar-data thunar-volman thunar xfce4-terminal thunar-media-tags-plugin thunar-archive-plugin xfce4-whiskermenu-plugin
 
 sudo apt --no-install-recommends install -y libreoffice-writer libreoffice-gtk3 -y
 sudo apt install -y libreoffice-l10n-pt-br -y
