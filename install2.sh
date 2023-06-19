c7901a2d341b2d14fd1fb6597776f9ed4f92e7aa
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-19 15:31:55 -0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -12,8 +12,8 @@ sudo apt install -y xserver-xorg-core xfonts-base xinit x11-xserver-utils xserve
 sudo apt install -y xfwm4 xfce4-panel xfce4-settings xfce4-session xfce4-terminal xfdesktop4 lxtask xfce4-screenshooter xfce4-power-manager gtk2-engines policykit-1 gtk2-engines-pixbuf
 sudo apt install -y xarchiver thunar-data thunar-volman thunar thunar-media-tags-plugin thunar-archive-plugin xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin
 
-#sudo apt install -y libreoffice-writer libreoffice-calc libreoffice-gtk3 -y
-#sudo apt install -y libreoffice-l10n-pt-br -y
+sudo apt install -y libreoffice-writer libreoffice-calc libreoffice-gtk3 -y
+sudo apt install -y libreoffice-l10n-pt-br -y
 
 mv /etc/apt/apt.conf /etc/apt/apt.conf.a
 
@@ -26,48 +26,48 @@ sudo apt update
 sudo apt install -y bluez pulseaudio-module-bluetooth thunderbird qbittorrent vlc atril blueman libgtk-3-dev
 sudo apt install -y lightdm lightdm-gtk-greeter
 
-#sudo apt update
+sudo apt update
 
-#sudo apt install -y virtualbox
-#sudo apt install -y deepin-deb-installer cockpit
-#sudo systemctl enable --now cockpit.socket
-#sudo usermod -aG sudo and
+sudo apt install -y virtualbox
+sudo apt install -y deepin-deb-installer cockpit
+sudo systemctl enable --now cockpit.socket
+sudo usermod -aG sudo and
 
 sudo apt update
 
-sudo apt install -y network-manager-gnome
+#sudo apt install -y network-manager-gnome
 
 sudo apt update
 
-#sudo apt install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon
-#sudo apt update
-#sudo apt install -y virt-manager
-#sudo usermod -aG kvm and
-#sudo usermod -aG libvirt and
-#sudo systemctl enable --now libvirtd
-#sudo systemctl start libvirtd
-#sudo apt --fix-broken install
+sudo apt install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon
+sudo apt update
+sudo apt install -y virt-manager
+sudo usermod -aG kvm and
+sudo usermod -aG libvirt and
+sudo systemctl enable --now libvirtd
+sudo systemctl start libvirtd
+sudo apt --fix-broken install
 
-#sudo apt update
-#sudo apt install -y linux-firmware
-#sudo apt update
+sudo apt update
+sudo apt install -y linux-firmware
+sudo apt update
 
-#wget https://github.com/balena-io/etcher/releases/download/v1.18.4/balena-etcher_1.18.4_amd64.deb -O balena.deb
-#sudo dpkg -i balena.deb
-#sudo apt --fix-broken install
+wget https://github.com/balena-io/etcher/releases/download/v1.18.4/balena-etcher_1.18.4_amd64.deb -O balena.deb
+sudo dpkg -i balena.deb
+sudo apt --fix-broken install
 
-#sudo apt update
+sudo apt update
 
-#sudo apt install flatpak
-#sudo apt --fix-broken install
-#sudo add-apt-repository ppa:flatpak/stable
-#sudo apt update
-#sudo apt install flatpak
-#sudo apt install gnome-software-plugin-flatpak
-#sudo apt update
-#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-#sudo apt --fix-broken install
-#sudo apt update
+sudo apt install flatpak
+sudo apt --fix-broken install
+sudo add-apt-repository ppa:flatpak/stable
+sudo apt update
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+sudo apt update
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo apt --fix-broken install
+sudo apt update
 
 sudo chmod 777 /usr/share/themes/
 sudo chmod 777 /usr/share/backgrounds/
@@ -83,7 +83,7 @@ tar -xvf xf/wallpapers2.tar.gz -C /usr/share/backgrounds/xfce/
 #https://download847.mediafire.com/blo2x1xe211goNdXcm_OR6xJTipOcelw36BAlRB9esc9Y7z4Wkg-lfKMeFvF1kOCMwpEaPbMqCb4yAlp5tQoESQgxjYDWsLTPPPyZnrNpHY8dLaXJm8YZYc34QhU7R3gT3Skl-_5WdRj5D5BSu4W5W85T4erpc19vpKMoa806emz2AI/uhiqf4jf8ldlm33/kora.tar.gz
 #tar -xvf kora.tar.gz -C /usr/share/icons/
 
-#sudo apt update
+sudo apt update
 
 mv /etc/apt/apt.conf.a /etc/apt/apt.conf
 
