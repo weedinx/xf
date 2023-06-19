9cb138e1cc0a4e509351beee2a358f15c11b4dc4
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-19 14:28:47 -0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -9,8 +9,8 @@ cp xf/apt.conf /etc/apt/
 sudo apt install -y xserver-xorg xserver-xorg-core xfonts-base xinit x11-xserver-utils
 sudo apt install -y xfce4 xfce4-terminal thunar-archive-plugin xfce4-whiskermenu-plugin thunar-volman
 
-sudo apt install -y libreoffice-writer libreoffice-calc libreoffice-gtk3 -y
-sudo apt install -y libreoffice-l10n-pt-br -y
+#sudo apt install -y libreoffice-writer libreoffice-calc libreoffice-gtk3 -y
+#sudo apt install -y libreoffice-l10n-pt-br -y
 
 mv /etc/apt/apt.conf /etc/apt/apt.conf.a
 
@@ -27,12 +27,12 @@ sudo apt update
 sudo apt install -y bluez pulseaudio-module-bluetooth thunderbird qbittorrent vlc atril blueman libgtk-3-dev
 sudo apt install -y lightdm lightdm-gtk-greeter
 
-sudo apt update
+#sudo apt update
 
-sudo apt install -y virtualbox
-sudo apt install -y deepin-deb-installer cockpit
-sudo systemctl enable --now cockpit.socket
-sudo usermod -aG sudo and
+#sudo apt install -y virtualbox
+#sudo apt install -y deepin-deb-installer cockpit
+#sudo systemctl enable --now cockpit.socket
+#sudo usermod -aG sudo and
 
 sudo apt update
 
@@ -40,36 +40,35 @@ sudo apt install -y network-manager-gnome
 
 sudo apt update
 
-sudo apt install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon
-sudo apt update
-sudo apt install -y virt-manager
-#sudo usermod -G libvirt -a and
-sudo usermod -aG kvm and
-sudo usermod -aG libvirt and
-sudo systemctl enable --now libvirtd
-sudo systemctl start libvirtd
-sudo apt --fix-broken install
+#sudo apt install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon
+#sudo apt update
+#sudo apt install -y virt-manager
+#sudo usermod -aG kvm and
+#sudo usermod -aG libvirt and
+#sudo systemctl enable --now libvirtd
+#sudo systemctl start libvirtd
+#sudo apt --fix-broken install
 
-sudo apt update
-sudo apt install -y linux-firmware
-sudo apt update
+#sudo apt update
+#sudo apt install -y linux-firmware
+#sudo apt update
 
-wget https://github.com/balena-io/etcher/releases/download/v1.18.4/balena-etcher_1.18.4_amd64.deb -O balena.deb
-sudo dpkg -i balena.deb
-sudo apt --fix-broken install
+#wget https://github.com/balena-io/etcher/releases/download/v1.18.4/balena-etcher_1.18.4_amd64.deb -O balena.deb
+#sudo dpkg -i balena.deb
+#sudo apt --fix-broken install
 
-sudo apt update
+#sudo apt update
 
-sudo apt install flatpak
-sudo apt --fix-broken install
-sudo add-apt-repository ppa:flatpak/stable
-sudo apt update
-sudo apt install flatpak
-sudo apt install gnome-software-plugin-flatpak
-sudo apt update
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo apt --fix-broken install
-sudo apt update
+#sudo apt install flatpak
+#sudo apt --fix-broken install
+#sudo add-apt-repository ppa:flatpak/stable
+#sudo apt update
+#sudo apt install flatpak
+#sudo apt install gnome-software-plugin-flatpak
+#sudo apt update
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#sudo apt --fix-broken install
+#sudo apt update
 
 sudo chmod 777 /usr/share/themes/
 sudo chmod 777 /usr/share/backgrounds/
@@ -82,10 +81,10 @@ sudo apt update
 tar -xvf xf/wallpapers1.tar.gz -C /usr/share/backgrounds/xfce/
 tar -xvf xf/wallpapers2.tar.gz -C /usr/share/backgrounds/xfce/
 
-https://download847.mediafire.com/blo2x1xe211goNdXcm_OR6xJTipOcelw36BAlRB9esc9Y7z4Wkg-lfKMeFvF1kOCMwpEaPbMqCb4yAlp5tQoESQgxjYDWsLTPPPyZnrNpHY8dLaXJm8YZYc34QhU7R3gT3Skl-_5WdRj5D5BSu4W5W85T4erpc19vpKMoa806emz2AI/uhiqf4jf8ldlm33/kora.tar.gz
-tar -xvf kora.tar.gz -C /usr/share/icons/
+#https://download847.mediafire.com/blo2x1xe211goNdXcm_OR6xJTipOcelw36BAlRB9esc9Y7z4Wkg-lfKMeFvF1kOCMwpEaPbMqCb4yAlp5tQoESQgxjYDWsLTPPPyZnrNpHY8dLaXJm8YZYc34QhU7R3gT3Skl-_5WdRj5D5BSu4W5W85T4erpc19vpKMoa806emz2AI/uhiqf4jf8ldlm33/kora.tar.gz
+#tar -xvf kora.tar.gz -C /usr/share/icons/
 
-sudo apt update
+#sudo apt update
 
 mv /etc/apt/apt.conf.a /etc/apt/apt.conf
 
