25b9eaf2e6bb6451bf43ae952d94d863d6a3f3fd
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-20 12:27:47 -0300

    Update db-install.sh

--- db-install.sh
+++ db-install.sh
@@ -1,77 +1,77 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
+apt update && sudo apt upgrade -y
 
 apt install firmware-linux-nonfree firmware-iwlwifi firmware-atheros firmware-realtek -y
 
-sudo apt install -y zip unzip tar gzip p7zip-full p7zip-rar
+apt install -y zip unzip tar gzip p7zip-full p7zip-rar
 
 cp xf/apt.conf /etc/apt/
 
-sudo apt update
+apt update
 
-sudo apt install -y xserver-xorg-core xfonts-base xinit x11-xserver-utils xserver-xorg
-sudo apt install -y xfwm4 xfce4-panel xfce4-settings xfce4-session xfce4-terminal xfdesktop4 lxtask xfce4-screenshooter xfce4-power-manager gtk2-engines policykit-1 gtk2-engines-pixbuf
-sudo apt install -y xarchiver thunar-data thunar-volman thunar thunar-media-tags-plugin thunar-archive-plugin xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin
+apt install -y xserver-xorg-core xfonts-base xinit x11-xserver-utils xserver-xorg
+apt install -y xfwm4 xfce4-panel xfce4-settings xfce4-session xfce4-terminal xfdesktop4 lxtask xfce4-screenshooter xfce4-power-manager gtk2-engines policykit-1 gtk2-engines-pixbuf
+apt install -y xarchiver thunar-data thunar-volman thunar thunar-media-tags-plugin thunar-archive-plugin xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin
 
-sudo apt install -y libreoffice-writer libreoffice-calc libreoffice-gtk3 -y
-sudo apt install -y libreoffice-l10n-pt-br -y
+apt install -y libreoffice-writer libreoffice-calc libreoffice-gtk3 -y
+apt install -y libreoffice-l10n-pt-br -y
 
 mv /etc/apt/apt.conf /etc/apt/apt.conf.a
 
-sudo apt update
+apt update
 
-sudo apt install -y gvfs gvfs-fuse gvfs-backends udisks2
-sudo apt install -y dkms curl git htop viewnior cpu-x gpick greybird-gtk-theme pulseaudio cups system-config-printer printer-driver-gutenprint printer-driver-escpr cups-client hplip
-sudo apt update
+apt install -y gvfs gvfs-fuse gvfs-backends udisks2
+apt install -y dkms curl git htop viewnior cpu-x gpick greybird-gtk-theme pulseaudio cups system-config-printer printer-driver-gutenprint printer-driver-escpr cups-client hplip
+apt update
 
-sudo apt install -y bluez pulseaudio-module-bluetooth thunderbird qbittorrent vlc atril blueman libgtk-3-dev
-sudo apt install -y lightdm lightdm-gtk-greeter
+apt install -y bluez pulseaudio-module-bluetooth thunderbird qbittorrent vlc atril blueman libgtk-3-dev
+apt install -y lightdm lightdm-gtk-greeter
 
-sudo apt update
+apt update
 
-sudo apt install -y virtualbox
-sudo apt install -y virtualbox-ext-pack
-sudo apt install -y deepin-deb-installer cockpit
-sudo systemctl enable --now cockpit.socket
+apt install -y virtualbox
+apt install -y virtualbox-ext-pack
+apt install -y deepin-deb-installer cockpit
+systemctl enable --now cockpit.socket
 sudo usermod -aG sudo and
 
 sudo apt update
 
-sudo apt install -y qemu-system libvirt-daemon-system
-sudo usermod -aG kvm and
-sudo usermod -aG libvirt and
-sudo apt update
-sudo apt install -y virt-manager
-sudo systemctl enable --now libvirtd
-sudo systemctl start libvirtd
-sudo apt --fix-broken install
+apt install -y qemu-system libvirt-daemon-system
+usermod -aG kvm and
+usermod -aG libvirt and
+apt update
+apt install -y virt-manager
+systemctl enable --now libvirtd
+systemctl start libvirtd
+apt --fix-broken install
 
-sudo apt update
-sudo apt install -y linux-firmware
-sudo apt update
+apt update
+apt install -y linux-firmware
+apt update
 
 wget https://github.com/balena-io/etcher/releases/download/v1.18.4/balena-etcher_1.18.4_amd64.deb -O balena.deb
-sudo dpkg -i balena.deb
-sudo apt --fix-broken install
+dpkg -i balena.deb
+apt --fix-broken install
 
-sudo apt update
+apt update
 
-sudo apt install flatpak
-sudo apt --fix-broken install
-sudo apt update
+apt install flatpak
+apt --fix-broken install
+apt update
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo apt --fix-broken install
-sudo apt install gnome-software-plugin-flatpak
-sudo apt update
+apt --fix-broken install
+apt install gnome-software-plugin-flatpak
+apt update
 
 
-sudo chmod 777 /usr/share/themes/
-sudo chmod 777 /usr/share/backgrounds/
-sudo chmod 777 /usr/share/backgrounds/xfce/
-sudo chmod 777 /usr/share/icons/
+chmod 777 /usr/share/themes/
+chmod 777 /usr/share/backgrounds/
+chmod 777 /usr/share/backgrounds/xfce/
+chmod 777 /usr/share/icons/
 
-sudo apt update
+apt update
 
 tar -xvf xf/wallpapers1.tar.gz -C /usr/share/backgrounds/xfce/
 tar -xvf xf/wallpapers2.tar.gz -C /usr/share/backgrounds/xfce/
@@ -81,21 +81,21 @@ tar -xvf xf/wallpapers2.tar.gz -C /usr/share/backgrounds/xfce/
 
 mv /etc/apt/apt.conf.a /etc/apt/apt.conf
 
-sudo apt update
+apt update
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
-sudo dpkg -i chrome.deb
-sudo apt --fix-broken install
+dpkg -i chrome.deb
+apt --fix-broken install
 
 wget https://az764295.vo.msecnd.net/stable/b3e4e68a0bc097f0ae7907b217c1119af9e03435/code_1.78.2-1683731010_amd64.deb -O vscode.deb
-sudo dpkg -i vscode.deb
-sudo apt --fix-broken install
+dpkg -i vscode.deb
+apt --fix-broken install
 
-#sudo apt install -y network-manager-gnome
+#apt install -y network-manager-gnome
 
-sudo apt update
+apt update
 
-sudo apt install -y nm-tray
+apt install -y nm-tray
 
 timedatectl set-timezone America/Sao_Paulo
 systemctl restart systemd-timedated
@@ -103,6 +103,6 @@ systemctl restart systemd-timedated
 systemctl disable systemd-networkd-wait-online.service
 systemctl mask systemd-networkd-wait-online.service
 
-sudo apt update && sudo apt upgrade -y
+apt update && sudo apt upgrade -y
 
-sudo shutdown -r now
+shutdown -r now
