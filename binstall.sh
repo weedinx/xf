8d502f7a4bc798ab30b7e33beb91b0215ccdfad2
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-23 21:28:56 -0300

    Update binstall.sh

--- binstall.sh
+++ binstall.sh
@@ -9,7 +9,7 @@ apt install -y zip tar gzip p7zip-full
 cp xf/apt.conf /etc/apt/
 cp xf/sudoers /etc/
 
-apt install -y libxfce4ui-utils thunar thunar-data thunar-volman thunar-media-tags-plugin thunar-archive-plugin xfce4-pulseaudio-plugin xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfdesktop4 xfwm4 xfce4-screenshooter xfce4-power-manager xfce4-whiskermenu-plugin x11-xserver-utils tumbler
+apt install -y xarchiver libxfce4ui-utils thunar thunar-data thunar-volman thunar-media-tags-plugin thunar-archive-plugin xfce4-pulseaudio-plugin xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfdesktop4 xfwm4 xfce4-screenshooter xfce4-power-manager xfce4-whiskermenu-plugin x11-xserver-utils tumbler
 apt install -y lightdm
     
 #mv etc/apt/apt.conf /etc/apt/apt.conf.a
