972c4eef452b9b4ff2d18c0f63273daa6401e4f7
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-25 16:33:43 -0300

    Update ub-install.sh

--- ub-install.sh
+++ ub-install.sh
@@ -8,7 +8,6 @@ cp xf/apt.conf /etc/apt/
 
 apt update
 
-#cp xf/sudoers /etc/
 apt install -y xorg
 apt install -y xarchiver libxfce4ui-utils thunar thunar-data thunar-volman thunar-media-tags-plugin thunar-archive-plugin xfce4-pulseaudio-plugin xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfdesktop4 xfwm4 xfce4-screenshooter xfce4-power-manager xfce4-whiskermenu-plugin x11-xserver-utils tumbler
 apt install -y lightdm
