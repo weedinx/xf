0c5e06c35cbc30c47ccda4e66af01e06e1a119bb
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-27 01:45:30 -0300

    Update cinna-install.sh

--- cinna-install.sh
+++ cinna-install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 apt update && apt -y upgrade
+
 sudo apt install -y network-manager
 apt install -y zip unzip tar gzip p7zip-full
 
 cp xf/apt.conf /etc/apt/
+
+apt update
 
 apt update
 apt install -y xorg
@@ -21,12 +24,6 @@ sudo apt install -y libvirt-clients libvirt-daemon-system libvirt-daemon virtins
 sudo apt install -y virt-manager
 
 apt update
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
-dpkg -i chrome.deb
-apt --fix-broken install
-
-#mkdir /usr/share/icons/kora
-#unzip xf/kora.zip -d /usr/share/icons/kora/
 
 sudo dpkg-reconfigure locales
 apt install -y language-pack-pt
