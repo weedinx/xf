7401e15c09514aa36fcd311e3362e3039c73be98
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-27 00:53:26 -0300

    Update cinna-install.sh

--- cinna-install.sh
+++ cinna-install.sh
@@ -8,17 +8,21 @@ cp xf/apt.conf /etc/apt/
 
 apt update
 apt install -y xorg
-apt install -y cinnamon-session
-apt install -y nemo nemo-fileroller nemo-terminal
+apt install -y cinnamon-core
+apt install -y network-manager-gnome
     
 rm -rf /etc/apt/apt.conf
 
 apt update
 apt install -y lightdm
-apt install -y network-manager-gnome
 #sudo apt install -y libvirt-clients libvirt-daemon-system libvirt-daemon virtinst bridge-utils qemu qemu-kvm
 #sudo apt install -y virt-manager
 #sudo apt install -y greybird-gtk-theme adwaita-icon-theme-full
+
+apt update
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
+dpkg -i chrome.deb
+apt --fix-broken install
 
 #mkdir /usr/share/icons/kora
 #unzip xf/kora.zip -d /usr/share/icons/kora/
