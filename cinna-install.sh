1160a3d764b6e4e08d8ab8f6ac05843ba50d4537
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-27 01:43:20 -0300

    Update cinna-install.sh

--- cinna-install.sh
+++ cinna-install.sh
@@ -10,6 +10,7 @@ apt update
 apt install -y xorg
 apt install -y cinnamon-core
 apt install -y network-manager-gnome
+apt install -y nemo-fileroller cinnamon-l10n
     
 rm -rf /etc/apt/apt.conf
 
@@ -18,8 +19,6 @@ apt install -y lightdm lightdm-gtk-greeter
 
 sudo apt install -y libvirt-clients libvirt-daemon-system libvirt-daemon virtinst bridge-utils qemu qemu-kvm
 sudo apt install -y virt-manager
-
-sudo apt install -y greybird-gtk-theme adwaita-icon-theme-full
 
 apt update
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
