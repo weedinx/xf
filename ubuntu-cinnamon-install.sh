771b7b73d412f825f114d1efe78674afc182238e
Anderson Dias <40615089+weedinx@users.noreply.github.com>
2023-06-28 09:33:22 -0300

    Update ubuntu-cinnamon-install.sh

--- ubuntu-cinnamon-install.sh
+++ ubuntu-cinnamon-install.sh
@@ -16,16 +16,12 @@ apt install -y network-manager
 apt install -y nemo nemo-fileroller cinnamon-l10n
 apt install -y zip unzip tar gzip p7zip-full
 
-#cp xf/apt1.conf /etc/apt/
-#mv xf/apt1.conf /etc/apt/apt.conf
-
 cp xf/apt.conf /etc/apt/
 apt update
 apt install -y network-manager-gnome
 apt install -y cinnamon-core
 apt install -y cinnamon-control-center-goa
-#apt install -y metacity-common
-#apt install -y network-manager-gnome
+apt install -y metacity-common
     
 rm -rf /etc/apt/apt.conf
 
